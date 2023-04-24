d6d1f047a133e30bb55c7f61cce7c58c08fcfa07
rmkkmr17 <ramkum94@tutanota.com>
2023-04-24 14:08:32 +0200

    Improved styling

--- src/loadPage.js
+++ src/loadPage.js
@@ -2,10 +2,13 @@ const loadPage = () => {
   const content = document.querySelector("#content");
   const headline = document.createElement("h1");
   const p = document.createElement("p");
+  const footer = document.createElement("footer");
   headline.textContent = "Vesuvio";
   p.textContent = "Italian food, at its very best.";
+  footer.textContent = `© Vesuvio 2023`;
   content.append(headline);
   content.append(p);
+  content.append(footer);
   return content;
 };
 export default loadPage;
